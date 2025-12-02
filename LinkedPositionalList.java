22460f0cca1d5f875a3876798c25641a3cb934f6
Suhaybui <114768604+Suhaybui@users.noreply.github.com>
2025-12-01 20:43:03 -0800

    Implement LinkedPositionalList class with basic operations

---
+++ LinkedPositionalList.java
@@ -0,0 +1,138 @@
+import java.util.Iterator;
+
+public class LinkedPositionalList<E> implements Iterable<E> {
+
+    private static class Node<E> implements Position<E> {
+        private E element;
+        private Node<E> prev;
+        private Node<E> next;
+
+        public Node(E e, Node<E> p, Node<E> n) {
+            element = e;
+            prev = p;
+            next = n;
+        }
+
+        public E getElement() {
+            return element;
+        }
+
+        public Node<E> getPrev() {
+            return prev;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setPrev(Node<E> p) {
+            prev = p;
+        }
+
+        public void setNext(Node<E> n) {
+            next = n;
+        }
+
+        public void setElement(E e) {
+            element = e;
+        }
+    }
+
+    private Node<E> header;
+    private Node<E> trailer;
+    private int size = 0;
+
+    public LinkedPositionalList() {
+        header = new Node<>(null, null, null);
+        trailer = new Node<>(null, header, null);
+        header.setNext(trailer);
+    }
+
+    private Node<E> validate(Position<E> p) {
+        return (Node<E>) p;
+    }
+
+    private Position<E> position(Node<E> node) {
+        if (node == header || node == trailer) return null;
+        return node;
+    }
+
+    public Position<E> first() {
+        return position(header.getNext());
+    }
+
+    public Position<E> last() {
+        return position(trailer.getPrev());
+    }
+
+    public Position<E> before(Position<E> p) {
+        Node<E> node = validate(p);
+        return position(node.getPrev());
+    }
+
+    public Position<E> after(Position<E> p) {
+        Node<E> node = validate(p);
+        return position(node.getNext());
+    }
+
+    private Position<E> addBetween(E e, Node<E> before, Node<E> after) {
+        Node<E> newNode = new Node<>(e, before, after);
+        before.setNext(newNode);
+        after.setPrev(newNode);
+        size++;
+        return newNode;
+    }
+
+    public Position<E> addFirst(E e) {
+        return addBetween(e, header, header.getNext());
+    }
+
+    public Position<E> addLast(E e) {
+        return addBetween(e, trailer.getPrev(), trailer);
+    }
+
+    public Position<E> addBefore(Position<E> p, E e) {
+        Node<E> node = validate(p);
+        return addBetween(e, node.getPrev(), node);
+    }
+
+    public Position<E> addAfter(Position<E> p, E e) {
+        Node<E> node = validate(p);
+        return addBetween(e, node, node.getNext());
+    }
+
+    public E set(Position<E> p, E e) {
+        Node<E> node = validate(p);
+        E old = node.getElement();
+        node.setElement(e);
+        return old;
+    }
+
+    public E remove(Position<E> p) {
+        Node<E> node = validate(p);
+        Node<E> prev = node.getPrev();
+        Node<E> next = node.getNext();
+        prev.setNext(next);
+        next.setPrev(prev);
+        size--;
+        return node.getElement();
+    }
+
+    private class ElementIterator implements Iterator<E> {
+        Position<E> cursor = first();
+
+        public boolean hasNext() {
+            return cursor != null;
+        }
+
+        public E next() {
+            E value = cursor.getElement();
+            cursor = after(cursor);
+            return value;
+        }
+    }
+
+    public Iterator<E> iterator() {
+        return new ElementIterator();
+    }
+}
